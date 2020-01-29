4ca11a3b22153ab9f631a3724eca4204563e6324
alainDuguine <alain.duguine@gmail.com>
2020-01-29 15:10:30 +0100

    Essaie d'integration avec Jacoco

--- myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImpIT.java
+++ myerp-consumer/src/test/java/com/dummy/myerp/consumer/dao/impl/db/dao/ComptabiliteDaoImpIT.java
@@ -18,7 +18,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:com/dummy/myerp/testconsumer/consumer/testContext.xml")
-public class ComptabiliteDaoImpTest {
+public class ComptabiliteDaoImpIT {
 
     @Autowired
     private ComptabiliteDaoImpl dao;
